d22956b6cc8c72cfac535057ccbdcf1b88cd2234
Amal <amalgovinds.mec@gmail.com>
2024-10-25 23:36:18 +0530

    PostGres DB Setup

--- mysite/settings.py
+++ mysite/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'tracker.apps.TrackerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'), 
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'), 
+        'PORT': config('DB_PORT'),
     }
 }
 
--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    task_name = models.CharField(max_length=200)
+
+class SubTask(models.Model):
+    parent_task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    subtask_name = models.CharField(max_length=200)
+
+class Date(models.Model):
+    parent_subtask = models.ForeignKey(SubTask, on_delete=models.CASCADE)
+    date = models.DateTimeField("Date of Execution")
+    status = models.BooleanField()
