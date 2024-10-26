d5bcd4d9ea885eee1b0ca9cdda49638bc00097a3
Amal <amalgovinds.mec@gmail.com>
2024-10-26 10:20:48 +0530

    Basic Models Seetup

--- tracker/models.py
+++ tracker/models.py
@@ -4,11 +4,20 @@ from django.db import models
 class Task(models.Model):
     task_name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.task_name
+
 class SubTask(models.Model):
     parent_task = models.ForeignKey(Task, on_delete=models.CASCADE)
     subtask_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.subtask_name
 
 class Date(models.Model):
     parent_subtask = models.ForeignKey(SubTask, on_delete=models.CASCADE)
     date = models.DateTimeField("Date of Execution")
     status = models.BooleanField()
+
+    def __str__(self):
+        return str(self.date.date())
