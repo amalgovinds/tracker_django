e6ae799a6ebdd7c92096fef1af1506f362a8dd23
Amal <amalgovinds.mec@gmail.com>
2024-10-29 22:07:25 +0530

    Basic view updated

--- tracker/views.py
+++ tracker/views.py
@@ -1,7 +1,11 @@
+from django.core import serializers
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
+from .models import Task, SubTask, Date
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the tracker index.")
+    task_list = Task.objects.all()
+    data = serializers.serialize('json', task_list)
+    return HttpResponse(data, content_type='application/json')
