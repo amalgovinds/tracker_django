ecb66c43e7d999e7f62815de6deeade8e858fdec
Amal <amalgovinds.mec@gmail.com>
2025-08-14 00:27:49 +0530

    chore: update todo

--- todo/models.py
+++ todo/models.py
@@ -3,9 +3,12 @@ from django.db import models
 
 # Create your models here.
 class Task(models.Model):
-    name = models.CharField(max_length=200)
-    date_created = models.DateTimeField("Date of Creation", null=True, blank=True)
-    is_completed = models.BooleanField(default=False)
+    name = models.CharField(max_length=200, verbose_name="Task Name")
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name="Date of Creation"
+    )
+    is_completed = models.BooleanField(default=False, verbose_name="Completed Status")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Last Updated")
 
     def __str__(self):
         return self.name
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from todo.models import Task
+from todo.serializers import TaskSerializer
 
 # Create your views here.
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    """
+    This ViewSet automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+    """
+
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
